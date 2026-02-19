729b8b95a20aa89c83bc4dd5c195ca11da7d7286
omkotawala-bacancy <om.kotawala@bacancy.us>
2026-02-19 16:59:40 +0530

    Change in groupByKey

--- NamasteDev/Easy/groupByKey.js
+++ NamasteDev/Easy/groupByKey.js
@@ -2,6 +2,7 @@ function groupBy(arr, key) {
     return arr.reduce((acc, val) => {
 
         const ourkey = val[key]
+
         if (!acc[ourkey]) {
             acc[ourkey] = []
         }
@@ -9,8 +10,18 @@ function groupBy(arr, key) {
         return acc
     }, {})
 }
-console.log(groupBy([
-    { name: 'Alice', age: 25 },
-    { name: 'Bob', age: 30 },
-    { name: 'Charlie', age: 25 }
-],'age'))
+
+const people = [
+  { name: "Aarav Patel", age: 22, city: "Ahmedabad" },
+  { name: "Priya Sharma", age: 25, city: "Mumbai" },
+  { name: "Rahul Verma", age: 22, city: "Ahmedabad" },
+  { name: "Sneha Reddy", age: 24, city: "Hyderabad" },
+  { name: "Vikram Singh", age: 25, city: "Mumbai" },   
+  { name: "Neha Gupta", age: 27, city: "Pune" },
+  { name: "Karan Mehta", age: 24, city: "Hyderabad" },
+  { name: "Ananya Iyer", age: 22, city: "Chennai" },   
+  { name: "Rohit Das", age: 27, city: "Pune" },        
+  { name: "Meera Nair", age: 25, city: "Mumbai" }       
+];
+
+console.log(groupBy(people, "age"))
